a3459bae8c4f1a01e34e66f7d660a7a387c2fe54
Scott Gardner <sgardne@gmail.com>
2016-01-13 19:47:32 -0600

    some refactor

--- simple-config-editor.py
+++ simple-config-editor.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-"""merge a config change and commit it."""
+"""
+merge a config change and commit it.
 
+Requires config.cfg with your config snippet inside at root directory.
+"""
 import getpass
 from napalm import get_network_driver
 from jnpr.junos.exception import ConnectAuthError
@@ -9,7 +12,9 @@ from jnpr.junos.exception import ConnectAuthError
 def main(default="config.cfg", username="user", password="password"):
     """Open the device, merge the config and commit it."""
     driver = get_network_driver('junos')
-    device = driver('10.56.133.100', username, password)
+
+    device_ip = input("Device IP Address or DNS: ")
+    device = driver(device_ip, username, password)
 
     try:
         device.open()
@@ -18,11 +23,22 @@ def main(default="config.cfg", username="user", password="password"):
             " re-enter credentials.")
         username, password = get_creds()
 
+
     #merge the local config snip
     device.load_merge_candidate(filename=default)
+
+    # Add confirmation dialog here
+    # Show the diff. If the diff is blank, say there are no changes and move
+    # on.
+
+    print(device.compare_config())
     
-    commit_result = device.commit_config()
-    print(commit_result)
+    commit_answer = "y"
+    commit_answer = input("Commit? (Y/n): ")
+
+    if (commit_answer == "y" or commit_answer == "Y"):
+        commit_result = device.commit_config()
+        print(commit_result)
 
     device.close()
 
